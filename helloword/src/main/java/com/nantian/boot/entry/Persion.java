61ab476c2aa98a5dfb54e593bccbc83f727bc214
MrJoeQian <mrjoeqian@163.com>
2018-05-16 17:37:51 +0800

    配置文件

---
+++ helloword/src/main/java/com/nantian/boot/entry/Persion.java
@@ -0,0 +1,37 @@
+package com.nantian.boot.entry;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import org.hibernate.validator.constraints.Email;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@NoArgsConstructor // 无参构造器
+@AllArgsConstructor // 全参构造器
+@Data
+// chain=true set方法返回值为this 而不是void
+@Accessors(chain = true)
+@PropertySource(value = { "classpath:person.properties" })
+@Component
+@ConfigurationProperties(prefix = "persion")
+public class Persion implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Email
+	private String name;
+	private int age;
+	private Date birth;
+	private Map<String, String> map;
+	private List<Object> list;
+	private Dog dog;
+
+}
--- helloword/src/test/java/com/nantian/boot/HellowordApplicationTests.java
+++ helloword/src/test/java/com/nantian/boot/HellowordApplicationTests.java
@@ -2,15 +2,33 @@ package com.nantian.boot;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.nantian.boot.entry.Persion;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class HellowordApplicationTests {
-
+	
+	@Value("${name1}")
+	private String name1;
+	@Value("${name2}")
+	private String name2;
+	@Value("${name3}")
+	private String name3;
+	@Autowired
+	private Persion persion;
+	
 	@Test
 	public void contextLoads() {
+		System.out.println(name1);
+		System.out.println(name2);
+		System.out.println(name3);
+		
+		System.out.println(persion);
 	}
 
 }
