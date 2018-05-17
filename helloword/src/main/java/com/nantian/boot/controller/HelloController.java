38ad8e85e85c81f2b6254f5a7b9fbeba0606de7f
MrJoeQian <mrjoeqian@163.com>
2018-05-17 15:46:31 +0800

    idea提交测试

--- helloword/src/main/java/com/nantian/boot/controller/HelloController.java
+++ helloword/src/main/java/com/nantian/boot/controller/HelloController.java
@@ -1,8 +1,9 @@
 package com.nantian.boot.controller;
 
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@ImportResource(locations = "classpath:beans.xml")
 @RestController
 public class HelloController {
 	@GetMapping("hello")
--- helloword/src/test/java/com/nantian/boot/HellowordApplicationTests.java
+++ helloword/src/test/java/com/nantian/boot/HellowordApplicationTests.java
@@ -1,14 +1,16 @@
 package com.nantian.boot;
 
+import com.nantian.boot.entry.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.nantian.boot.entry.Person;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -24,7 +26,9 @@ public class HellowordApplicationTests {
 	private Person person;
 	@Autowired
 	private ApplicationContext alc;
-	
+
+	Logger logger = LoggerFactory.getLogger(getClass());
+
 	@Test
 	public void contextLoads() {
 		/*System.out.println(name1);
@@ -42,5 +46,18 @@ public class HellowordApplicationTests {
 	public void testHelloService2() {
 		
 		System.out.println(alc.containsBean("helloService2"));
+	}
+
+	@Test
+	public void testLog(){
+		//日志的级别：
+		//从高到低 trace<debug<info<warn<error
+		//日志只会输出设置的最小级别及其以上级别的日志
+		logger.trace("这是trace日志...");
+		logger.debug("这是debug日志...");
+		//StringBoot默认日志输出级别为info
+		logger.info("这是info日志...");
+		logger.warn("这是warn日志...");
+		logger.error("这是error日志...");
 	}
 }
